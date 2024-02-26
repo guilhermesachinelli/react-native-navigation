dd11f7155fca940d7a56f91e3991458f52cd62d9
guilherme godoy <guilhermesachinelli.564@gmail.com>
2024-02-26 10:45:55 -0300

    Feat: import navigations container

--- App.jsx
+++ App.jsx
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+// importando o NavigationContainer  para criar a navegação
+import { NavigationContainer } from '@react-navigation/native';
+// importando o createNativeStackNavigator para criar a navegação
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// home é a tela inicial
+import Home from './src/screens/Home';
+// contact é a tela de contato
+import Contact from './src/screens/Contact';
+// profile é a tela de perfil
+import Profile from './src/screens/Profile';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={Home} />
+        <Stack.Screen name="Contato" component={Contact} />
+        <Stack.Screen name="Perfil" component={Profile} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
